eb2717218be065526d40a42892a388da19d2ec2c
i-8086 <mighty8086@gmail.com>
2020-04-25 22:12:30 +0200

    startup

--- scripts/wmpicker.sh
+++ scripts/wmpicker.sh
@@ -4,13 +4,18 @@ echo
 echo "Select WM:"
 echo "1 - dwm"
 echo "2 - openbox"
-
+echo "3 - bspwm"
+echo "4 - spectrwm"
 while true; do
 	read -t 10 -p "Option (timeout 10s): [dwm]:" -e wm
 	case ${wm:=1} in
 		1) 
 			break 2;;
 		2) 
+			break 2;;
+		3)	
+			break 2;;
+		4)	
 			break 2;;
 		*) echo "Invalid input."
 	esac
